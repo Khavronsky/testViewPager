a9427ea32bcd4866642c9dd3283ea89b539dd9d1
khavronsky <khavronsky@gmail.com>
2017-01-12 19:03:40 +0300

    simple pager

--- app/src/main/java/com/khavronsky/testviewpager/MainActivity.java
+++ app/src/main/java/com/khavronsky/testviewpager/MainActivity.java
@@ -1,13 +1,23 @@
 package com.khavronsky.testviewpager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+    private ViewPager viewPager;
+    private MyFragmentAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter = new MyFragmentAdapter(getSupportFragmentManager());
+
+        viewPager = (ViewPager) findViewById(R.id.my_pager);
+        viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(1);
+
     }
 }
---
+++ app/src/main/java/com/khavronsky/testviewpager/MyFragmentAdapter.java
@@ -0,0 +1,29 @@
+package com.khavronsky.testviewpager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+class MyFragmentAdapter extends FragmentPagerAdapter {
+    public MyFragmentAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new Fragment1();
+            case 1:
+                return new Fragment2();
+            case 2:
+                return new Fragment3();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
